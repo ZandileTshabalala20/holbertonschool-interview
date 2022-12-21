1776c27370162f73b88d8667b9a49b0a231fc7cb
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-12-21 08:38:45 +0200

    commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+"""
+Python script takes URL from stdin and compute exact metrics
+"""
 
 
-import sys
+def printst(dictionary, size):
+    """
+    Print function
+    """
+    d = sorted(dictionary.keys())
+    print("File size: {:d}".format(size))
+    for i in d:
+        if dictionary[i] != 0:
+            print("{}: {:d}".format(i, dictionary[i]))
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+count = 0
+size = 0
+dic = {"200": 0, "301": 0, "400": 0, "401": 0,
+       "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+    with open(0) as f:
+        for line in f:
+            count += 1
+            arr = line.split()
+            try:
+                size += int(arr[-1])
+            except:
+                pass
+            try:
+                st = arr[-2]
+                if st in dic:
+                    dic[st] += 1
+            except:
+                pass
+            if count % 10 == 0:
+                printst(dic, size)
+        printst(dic, size)
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+except KeyboardInterrupt:
+    printst(dic, size)
+    raise
