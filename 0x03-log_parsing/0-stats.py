231400bc0470d3b82f15218dd723c464cdc4fab8
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-12-21 08:33:55 +0200

    commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,42 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
-- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-                code> <file size>
-- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-    statistics from the beginning:
-Example:
-    File size: 5213
-    200: 2
-    401: 1
-    403: 2
-    404: 1
-    405: 1
-    500: 3
-"""
+"""Python script that reads stdin line by line and computes metrics"""
+
 import sys
-stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-summ = 0
 
 
-def prn_stats():
-    """
-    Function that print stats about log
-    """
-    global summ
-
-    print('File size: {}'.format(summ))
-    stcdor = sorted(stcd.keys())
-    for each in stcdor:
-        if stcd[each] > 0:
-            print('{}: {}'.format(each, stcd[each]))
+def print_n(t_file_size, status):
+    """Prints total file size and status list"""
+    print("File size: {:d}".format(t_file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
-if __name__ == "__main__":
-    cnt = 0
-    try:
-        """ Iter the standar input """
-        for data in sys.stdin:
-            try:
-                fact = data.split(' ')
-                """ If there is a status code """
-                if fact[-2] in stcd:
-                    stcd[fact[-2]] += 1
-                """ If there is a lenght """
-                summ += int(fact[-1])
-            except:
-                pass
-            """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                prn_stats()
-                cnt = 0
-    except KeyboardInterrupt:
-        prn_stats()
-        raise
-    else:
-        prn_stats()
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+
+t_file_size = 0
+count = 0
+try:
+    for line in sys.stdin:
+        args = line.split()
+
+        if len(args) > 2:
+            status_code = args[-2]
+            file_size = int(args[-1])
+
+            if status_code in status:
+                status[status_code] += 1
+
+            t_file_size += file_size
+            count += 1
+
+            if count == 10:
+                print_n(t_file_size, status)
+                count = 0
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print_n(t_file_size, status)
